029bac5a30341c0b4bdbe87efec7c8e09c2f25b2
Shreyansh Jaiswal <jshreyansh459@gmail.com>
2023-05-03 00:54:09 +0530

    files edited

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,16 +1,61 @@
 import React from "react";
 import './RecommendedVideos.css'
+import VideoCard from "./VideoCard"
 
 function RecommendedVideos(){
     return (
         <div className="recommendedVideos">
             <h2>Recommended</h2>
-            <div className="recommendedVideoss__videos">
-                <Video />
-                <Video />
-                <Video />
-                <Video />
-                <Video />
+            <div className="recommendedVideos__videos">
+                <VideoCard 
+                title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
+                views="3.2M"
+                timestamp="5 days ago"
+                channelImage="https://yt3.ggpht.com/tnt5nlj6KmjZt3Bp4wy-v33jgM2nYVYtloqUiA3WAbjGgXGZqUui9I4dj_VlBxAw7pfWN0WG3A=s48-c-k-c0x00ffffff-no-rj"
+                channel="Aaditya Kulshreshth"
+                image="https://i.ytimg.com/vi/U80wUKOcLpE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDGbtJ8Mi3aV_7tFVkidYZieDvwDg"
+                />
+
+                <VideoCard 
+                title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
+                views="3.2M"
+                timestamp="5 days ago"
+                channelImage="https://yt3.ggpht.com/tnt5nlj6KmjZt3Bp4wy-v33jgM2nYVYtloqUiA3WAbjGgXGZqUui9I4dj_VlBxAw7pfWN0WG3A=s48-c-k-c0x00ffffff-no-rj"
+                channel="Aaditya Kulshreshth"
+                image="https://i.ytimg.com/vi/U80wUKOcLpE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDGbtJ8Mi3aV_7tFVkidYZieDvwDg"
+                />
+
+
+                <VideoCard 
+                title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
+                views="3.2M"
+                timestamp="5 days ago"
+                channelImage="https://yt3.ggpht.com/tnt5nlj6KmjZt3Bp4wy-v33jgM2nYVYtloqUiA3WAbjGgXGZqUui9I4dj_VlBxAw7pfWN0WG3A=s48-c-k-c0x00ffffff-no-rj"
+                channel="Aaditya Kulshreshth"
+                image="https://i.ytimg.com/vi/U80wUKOcLpE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDGbtJ8Mi3aV_7tFVkidYZieDvwDg"
+                />
+
+
+                <VideoCard 
+                title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
+                views="3.2M"
+                timestamp="5 days ago"
+                channelImage="https://yt3.ggpht.com/tnt5nlj6KmjZt3Bp4wy-v33jgM2nYVYtloqUiA3WAbjGgXGZqUui9I4dj_VlBxAw7pfWN0WG3A=s48-c-k-c0x00ffffff-no-rj"
+                channel="Aaditya Kulshreshth"
+                image="https://i.ytimg.com/vi/U80wUKOcLpE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDGbtJ8Mi3aV_7tFVkidYZieDvwDg"
+                />
+
+
+                <VideoCard 
+                title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
+                views="3.2M"
+                timestamp="5 days ago"
+                channelImage="https://yt3.ggpht.com/tnt5nlj6KmjZt3Bp4wy-v33jgM2nYVYtloqUiA3WAbjGgXGZqUui9I4dj_VlBxAw7pfWN0WG3A=s48-c-k-c0x00ffffff-no-rj"
+                channel="Aaditya Kulshreshth"
+                image="https://i.ytimg.com/vi/U80wUKOcLpE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDGbtJ8Mi3aV_7tFVkidYZieDvwDg"
+                />
+
+
             </div>
         </div>
     )
