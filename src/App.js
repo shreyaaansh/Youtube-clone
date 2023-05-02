1be70b7ebde0254525428ff7d89f592b8bb1be16
Shreyansh Jaiswal <jshreyansh459@gmail.com>
2023-05-02 18:02:00 +0530

    files edited

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-      <h1></h1>
-
       <Header />
+      <Sidebar /> 
+
+ 
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -10,19 +10,27 @@ import { Avatar } from "@mui/material";
 function Header() {
     return (
         <div className="header">
-            
-            <Menu />
-            <img 
-            className="header__logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
-            alt="" />
+            <div className="header__left">
+                <Menu />
+                <img     
+                className="header__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+                alt="" 
+                />
+            </div>
 
-            <input type="text" />
-            <Search />
-            <VideoCall />
-            <Apps />
-            <Notifications />
-            <Avatar img/>
+            <div className="header__input">
+                <input placeholder = "Search" type="text" />
+                <Search className="header__inputButton"/>
+            </div>
+            
+            <div className="header__icons">
+                <VideoCall className="header__icon"/>
+                <Apps className="header__icon"/>
+                <Notifications className="header__icon"/>
+                <Avatar img=""/>
+            </div>
+            
         </div>
     );
 }
