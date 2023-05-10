9a82dec1767007400216a01ce4c58f38092a2800
Shreyansh Jaiswal <jshreyansh459@gmail.com>
2023-05-10 20:33:28 +0530

    files added

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -17,6 +17,56 @@ function RecommendedVideos(){
                 />
 
                 <VideoCard 
+                title= "Complete Full Stack Web Developer Roadmap"
+                views="1.2M"
+                timestamp="3 monhs ago"
+                channelImage="hhttps://yt3.ggpht.com/ytc/AGIKgqPmVT6_YQd7RIhhoy9So5Jk9Iqw8pzivKCfLPm_Yg=s48-c-k-c0x00ffffff-no-rj"
+                channel="CodeWithHarry"
+                image="https://i.ytimg.com/vi/vSW2M-BB1NE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLAYrJmiYBa6eGIZfpqwwH2lX6C5-w"
+                />
+
+
+                <VideoCard 
+                title= "The Weeknd - I Was Never There feat. Gesaffelstein (Official Audio)"
+                views="382M"
+                timestamp="1 Year ago"
+                channelImage="https://yt3.ggpht.com/QiI-c4cFyRPD0qVwTQooC3dlgJqHA_t6CpEAv818om-mqL9bqNDL4L_qXQVXx_eY76D_7cLD=s48-c-k-c0x00ffffff-no-nd-rj"
+                channel="The Weeknd"
+                image="https://i.ytimg.com/vi/OlStmta0Vh4/hqdefault.jpg?sqp=-oaymwE1CKgBEF5IVfKriqkDKAgBFQAAiEIYAXABwAEG8AEB-AH-CYAC0AWKAgwIABABGH8gKSgTMA8=&rs=AOn4CLB4ez-PlILh1ZdgCunm4gCP_KKi4g"
+                />
+
+
+                <VideoCard 
+                title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
+                views="3.2M"
+                timestamp="5 days ago"
+                channelImage="https://yt3.ggpht.com/tnt5nlj6KmjZt3Bp4wy-v33jgM2nYVYtloqUiA3WAbjGgXGZqUui9I4dj_VlBxAw7pfWN0WG3A=s48-c-k-c0x00ffffff-no-rj"
+                channel="Aaditya Kulshreshth"
+                image="https://i.ytimg.com/vi/U80wUKOcLpE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDGbtJ8Mi3aV_7tFVkidYZieDvwDg"
+                />
+
+
+                <VideoCard 
+                title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
+                views="3.2M"
+                timestamp="5 days ago"
+                channelImage="https://yt3.ggpht.com/tnt5nlj6KmjZt3Bp4wy-v33jgM2nYVYtloqUiA3WAbjGgXGZqUui9I4dj_VlBxAw7pfWN0WG3A=s48-c-k-c0x00ffffff-no-rj"
+                channel="Aaditya Kulshreshth"
+                image="https://i.ytimg.com/vi/U80wUKOcLpE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDGbtJ8Mi3aV_7tFVkidYZieDvwDg"
+                />
+
+
+                <VideoCard 
+                title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
+                views="3.2M"
+                timestamp="5 days ago"
+                channelImage="https://yt3.ggpht.com/tnt5nlj6KmjZt3Bp4wy-v33jgM2nYVYtloqUiA3WAbjGgXGZqUui9I4dj_VlBxAw7pfWN0WG3A=s48-c-k-c0x00ffffff-no-rj"
+                channel="Aaditya Kulshreshth"
+                image="https://i.ytimg.com/vi/U80wUKOcLpE/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLDGbtJ8Mi3aV_7tFVkidYZieDvwDg"
+                />
+
+
+                <VideoCard 
                 title= "Mummy | Stand up Comedy | Aaditya Kulshreshth aka Kullu"
                 views="3.2M"
                 timestamp="5 days ago"
